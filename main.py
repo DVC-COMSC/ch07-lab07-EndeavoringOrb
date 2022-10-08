9bf45ba089f2813b71cafaff83307db5461cf2b7
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 14:49:47 -0700

    switch order of print statements

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ for i in range(0,len(numbers1),2):
 	numbers2.append(numbers1[i])
 for i in range(math.floor((len(numbers1)+1)/2)):
 	numbers1.pop(i)
-print(numbers1)
 print(numbers2)
+print(numbers1)
 #(1),2,3,4,5,6,7,8,9,10
 #2,(3),4,5,6,7,8,9,10
 #2,4,(5),6,7,8,9,10
