1d015debec7323c990fd2d9be2e39d47879377e8
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 14:22:13 -0700

    2nd commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 inputvalues = input('Enter all elements values: ').split()
 numbers1 = list(map(int, inputvalues))
 numbers2 = []
-for i in range(0,len(numbers1),2):
+for i in range(0,len(numbers1)-1,2):
 	numbers2.append(i)
 	numbers1.pop(i)
